9d56e2d3933963779c95249530182d309ec34177
Baptiste Clavié <clavie.b@gmail.com>
2017-08-01 11:13:07 +0200

    Add FosRestBundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'FOS\RestBundle\FOSRestBundle' => ['all' => true],
 ];
