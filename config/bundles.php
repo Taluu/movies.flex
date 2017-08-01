a4a55d10aaa690e592e552acc600706c1a348852
Baptiste Clavié <clavie.b@gmail.com>
2017-08-01 11:41:58 +0200

    Add doctrine configuration

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
     'FOS\RestBundle\FOSRestBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
 ];
